091a92210e2f1173cb48a0532006bbb0604134ab
quangtechno <148311798+quangtechno@users.noreply.github.com>
2024-11-26 10:20:56 +0700

    upload 26/11/2024 10:20

--- front-end/client/src/components/Income_Dashboard.jsx
+++ front-end/client/src/components/Income_Dashboard.jsx
@@ -91,14 +91,16 @@ const Income_DashBoard = () => {
                                         <tr key={index}>
                                         <td>
                                             <span className="custom-checkbox">
-                                                <input type="checkbox" id="checkbox1" name="options[]" value="1" />
+                                                <input type="checkbox" id="checkbox1" className="options[]" value="1" />
                                                 <label htmlFor="checkbox1"></label>
                                             </span>
                                         </td>
                                         <td>{index+1}</td>
                                         <td>{item.nameIncome}</td>
                                         <td>{item.amount}$</td>
-                                        <td>{item.user.username}</td>
+                                        {/* <td>{(item.user.username)?(item.user.username):("not found")}</td> */}
+                                        {item.user&&(item.user.username)?(<td>{item.user.username}</td>):(<td>Loading....</td>)}
+                                       
                                         <td>
                                             <a href="#editEmployeeModal" className="edit" onClick={(event) => { handleEditPopUp(event) }}><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                                             <a href="#deleteEmployeeModal" className="delete"  onClick={handleDeletePopUp}><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
