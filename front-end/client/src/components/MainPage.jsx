0f1c72b96c843960746ea4c69b669d7dc8b02436
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2024-11-29 15:40:42 +0700

    update

--- front-end/client/src/components/MainPage.jsx
+++ front-end/client/src/components/MainPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-// import axios from "axios"
 import "./CSS/MainPage.css";
 import DashBoard from "./DashBoard";
 import Sidebar from "./Sidebar";
@@ -7,21 +6,6 @@ const MainPage = () => {
 
   return (
     <div className="app">
-      {/* <div className="sidebar">
-        <div className="logo">
-          <img src="./image/finance.png" alt="logo"/>
-        </div>
-        <ul>
-          <li><a href=""><i className="bi bi-bar-chart-line-fill"></i> DASH BOARD</a></li>
-          <li><a href="/expense_tracking/home/income"><i class="fa-solid fa-money-check-dollar"></i> INCOME</a></li>
-          <li><a href=""><i class="fa-solid fa-money-bills"></i> EXPENSE</a></li>
-          <li><a href=""><i className="bi bi-wallet2"></i> TRANSACTIONS</a></li>
-          <li><a href=""><i className="bi bi-bell-fill"></i> NOTIFICATION</a></li>
-          <li><a href=""><i className="bi bi-gear-fill"></i> SETTING</a></li>
-          
-          
-        </ul>
-      </div> */}
       <Sidebar/>
       <div className="main-content">
         <h1 style={{ fontSize: "30px", fontWeight: "bold", width: "400px", wordWrap: "break-word" }}>Project Expense Tracking Software</h1>
